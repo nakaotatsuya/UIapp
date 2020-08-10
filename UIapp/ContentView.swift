0af29a554c7c93a71b3941e59f4ce84f36183c96
nakaotatsuya <earthdefense10@gmail.com>
2020-08-10 19:22:23 +0900

    Initial Commit

---
+++ UIapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIapp
+//
+//  Created by 中尾　達也 on 2020/08/10.
+//  Copyright © 2020 Sample Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
